252637f6f56d46f0c6911e79321d38d816035ef5
Droidbuster <coolboysuraj854@gmail.com>
2015-05-16 16:59:02 -1200

    Final Version GingerBread Platelogo Lollipop
    Hi friends.Well today i am going to publish my Droidbuster Lollipop
    Platelogo version v2
    
    Changelog
    
    fixed Color issue (Now Lollipop Color can able to change)
    Multiple density  Support...

---
+++ gen/com/Droidbuster/droidbuster_LollipopEasterEgg/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.Droidbuster.droidbuster_LollipopEasterEgg;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
